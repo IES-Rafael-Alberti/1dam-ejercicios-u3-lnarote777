a0fd366ad17559917cee04ac242ac446070b8cec
lnarote777 <nolara@iesrafaelalberti.es>
2023-11-22 18:38:39 +0100

    ej 2 conjuntos

--- src/práctica3_3/ej3_02.py
+++ src/práctica3_3/ej3_02.py
@@ -15,24 +15,49 @@ def borrarConsola():
 
 
 def finalizar():
-    print( 'Cuando desee finalizar introduzca x\n')
+    print( '---Cuando desee finalizar introduzca x---\n')
+
+
+def primariaIncluidosSecundaria(conjunto_primaria, conjunto_secundaria):
+    incluidos = conjunto_primaria <= conjunto_secundaria    
+    
+    print(f'¿Los nombres de primaria estan incluidos en secundaria? {incluidos}')
+
+
+def alumnosNoRepetidos(conjunto_primaria, conjunto_secundaria):
+    norepetidos = conjunto_primaria - conjunto_secundaria
+    print(f'\nNombres de primaria que no se repiten en secundaria: {norepetidos}')
+
+
+def alumnosRepetidos(conjunto_primaria, conjunto_secundaria):
+    repetidos = conjunto_primaria & conjunto_secundaria
+    print(f'\nNombres que se repiten en los dos cursos: {repetidos}')
 
 
 def crearConjunto():
     
     primaria = añadirAlumnosPrimaria()
-    secundaria = añadirAlumnosSecundaria()
     conjunto_primaria = set(primaria)
-    conjunto_secundaria = set(secundaria)
     print('Lista alumnado de primaria:\n', conjunto_primaria)
-    print('\nLista alumnado secundaria:\n', conjunto_secundaria)
+    
+    secundaria = añadirAlumnosSecundaria()
+    conjunto_secundaria = set(secundaria)
+    print('Lista alumnado secundaria:\n', conjunto_secundaria)
 
+    alumnosRepetidos(conjunto_primaria, conjunto_secundaria)
+    alumnosNoRepetidos(conjunto_primaria, conjunto_secundaria)
+    primariaIncluidosSecundaria(conjunto_primaria, conjunto_secundaria)
+    
 
 def añadirAlumnosPrimaria() -> list:
     finalizar()
+    
     print('---ALUMNADO PRIMARIA---')
+    
     alumnos_primaria = []
+    
     while True:
+        
         nombre_pila= input('Introduzca el nombre de pila del alumno: ')
         
         if nombre_pila.lower() == 'x':
@@ -46,8 +71,10 @@ def añadirAlumnosPrimaria() -> list:
         
 def añadirAlumnosSecundaria() -> list:       
 
-    print('---ALUMNADO SECUNDARIA---')
-    alumnos_secundaria =[]
+    print('\n---ALUMNADO SECUNDARIA---')
+    
+    alumnos_secundaria = []
+    
     while True:
         nombre_pila= input('Introduzca el nombre de pila del alumno: ')
         
